654f054182de182fa4f7fc37149b8f3a61390945
unknown <dyah_putri_26rpl@student.smktelkom-mlg.sch.id>
2019-02-13 08:14:41 +0700

    new

--- application/views/admin/_partials/sidebar.php
+++ application/views/admin/_partials/sidebar.php
@@ -6,6 +6,7 @@
             <span>Overview</span>
         </a>
     </li>
+
     <li class="nav-item dropdown <?php echo $this->uri->segment(2) == 'products' ? 'active': '' ?>">
         <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
             aria-expanded="false">
@@ -13,10 +14,35 @@
             <span>Products</span>
         </a>
         <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-            <a class="dropdown-item" href="<?php echo site_url('admin/products/new') ?>">New Product</a>
+            <a class="dropdown-item" href="<?php echo site_url('admin/products/add') ?>">New Product</a>
             <a class="dropdown-item" href="<?php echo site_url('admin/products') ?>">List Product</a>
         </div>
     </li>
+
+    <li class="nav-item dropdown <?php echo $this->uri->segment(2) == 'karyawan' ? 'active': '' ?>">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+            aria-expanded="false">
+            <i class="fas fa-fw fa-boxes"></i>
+            <span>customer</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <a class="dropdown-item" href="<?php echo site_url('admin/customer/add') ?>">New customer</a>
+            <a class="dropdown-item" href="<?php echo site_url('admin/customer') ?>">List customer</a>
+        </div>
+    </li>
+
+    <li class="nav-item dropdown <?php echo $this->uri->segment(2) == 'supplier' ? 'active': '' ?>">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+            aria-expanded="false">
+            <i class="fas fa-fw fa-boxes"></i>
+            <span>supplier</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <a class="dropdown-item" href="<?php echo site_url('admin/suppliers/add') ?>">New supplier</a>
+            <a class="dropdown-item" href="<?php echo site_url('admin/suppliers') ?>">List supplier</a>
+        </div>
+    </li>
+
     <li class="nav-item">
         <a class="nav-link" href="#">
             <i class="fas fa-fw fa-users"></i>
